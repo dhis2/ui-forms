5a1b89c2b4177c21b0a678d155d837eb8b173497
Jan-Gerke Salomon <jgs.salomon@gmail.com>
2019-08-08 15:35:46 +0200

    Initial commit

---
+++ .storybook/config.js
@@ -0,0 +1,19 @@
+import { configure, addDecorator } from '@storybook/react'
+import { jsxDecorator } from 'storybook-addon-jsx'
+import { withPropsTable } from 'storybook-addon-react-docgen'
+import { CssResetWrapper } from './css-reset-decorator'
+import { FrameStylesWrapper } from './frame-styles-decorator'
+
+import 'typeface-roboto'
+
+addDecorator(jsxDecorator)
+addDecorator(withPropsTable)
+addDecorator(CssResetWrapper)
+addDecorator(FrameStylesWrapper)
+
+function loadStories() {
+    const req = require.context('../stories', true, /\.stories\.js$/)
+    req.keys().forEach(filename => req(filename))
+}
+
+configure(loadStories, module)
---
+++ babel.config.js
@@ -0,0 +1,26 @@
+module.exports = function(api) {
+    api.cache.forever()
+
+    let defaultPresets
+
+    if (process.env.BABEL_ENV === 'modules') {
+        defaultPresets = []
+    } else {
+        defaultPresets = [
+            [
+                '@babel/preset-env',
+                {
+                    modules: 'commonjs',
+                },
+            ],
+        ]
+    }
+
+    return {
+        presets: defaultPresets.concat('@babel/preset-react'),
+        plugins: [
+            '@babel/plugin-proposal-class-properties',
+            'styled-jsx/babel',
+        ],
+    }
+}
