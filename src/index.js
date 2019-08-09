55e3a388c878276cea792264dea7e5c61c57bad2
Jan-Gerke Salomon <jgs.salomon@gmail.com>
2019-08-09 10:33:37 +0200

    refactor: reflect refactoring and new components in index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
-export * from './File.js'
+/**
+ * Form components
+ */
 export * from './Form.js'
+export * from './ManagedForm.js'
+
+/**
+ * Input components
+ */
+export { File, ConditionalFile } from './File.js'
+export { Radio } from './Radio.js'
+export { RadioGroup, ConditionalRadioGroup } from './RadioGroup.js'
+
+/**
+ * helpers
+ */
 export * from './validators.js'
