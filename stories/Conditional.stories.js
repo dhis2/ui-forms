d2f009197bd580700a84031551a44fe6c7a64512
Hendrik de Graaf <hendrik@dhis2.org>
2019-08-14 16:23:57 +0200

    chore(form): remove ManagedForm in favour of just using the RFF API

--- stories/Conditional.stories.js
+++ stories/Conditional.stories.js
@@ -3,63 +3,30 @@ import React from 'react'
 import { storiesOf } from '@storybook/react'
 
 import { Form, File, Conditional, clear } from '../src/index.js'
-import { ManagedForm } from '../src/ManagedForm.js'
 
-storiesOf('Conditional fields', module)
-    .add('Plain form', () => (
-        <Form
-            onSubmit={values => console.log(JSON.stringify(values, null, 2))}
-            mutators={{ clear }}
-        >
-            {({ onSubmit, values }) => (
-                <form onSubmit={onSubmit}>
-                    <File name="file1" label="File one" />
+storiesOf('Conditional fields', module).add('Default', () => (
+    <Form onSubmit={v => console.log(v)} mutators={{ clear }}>
+        {({ handleSubmit, values }) => (
+            <form onSubmit={handleSubmit}>
+                <File name="file1" label="File one" />
 
-                    <Conditional
-                        show={!!values.file1}
-                        fieldsToClearOnHide={['file3', 'file4']}
-                    >
-                        <File name="file3" label="File three" />
-                        <File name="file4" label="File four" />
-                        <File name="file5" label="File five" />
-                        <div>
-                            <p>
-                                Conditional blocks can contain random other
-                                elements too
-                            </p>
-                        </div>
-                    </Conditional>
+                <Conditional
+                    show={!!values.file1}
+                    fieldsToClearOnHide={['file2', 'file3']}
+                >
+                    <File name="file2" label="File two - cleared on hide" />
+                    <File name="file3" label="File three - cleared on hide" />
+                    <File name="file4" label="File four - not cleared" />
+                    <div>
+                        <p>
+                            Conditional blocks can contain random other elements
+                            too
+                        </p>
+                    </div>
+                </Conditional>
 
-                    <button type="submit">Submit</button>
-                </form>
-            )}
-        </Form>
-    ))
-    .add('Managed form', () => (
-        <ManagedForm
-            onSubmit={values => console.log(JSON.stringify(values, null, 2))}
-        >
-            {({ onSubmit, values }) => (
-                <React.Fragment>
-                    <File name="file1" label="File one" />
-
-                    <Conditional
-                        show={!!values.file1}
-                        fieldsToClearOnHide={['file3', 'file4']}
-                    >
-                        <File name="file3" label="File three" />
-                        <File name="file4" label="File four" />
-                        <File name="file5" label="File five" />
-                        <div>
-                            <p>
-                                Conditional blocks can contain random other
-                                elements too
-                            </p>
-                        </div>
-                    </Conditional>
-
-                    {/* <button type="submit">Submit</button> */}
-                </React.Fragment>
-            )}
-        </ManagedForm>
-    ))
+                <button type="submit">Submit</button>
+            </form>
+        )}
+    </Form>
+))
