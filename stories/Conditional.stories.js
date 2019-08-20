bbe4e7b4fc9d3d42cc17208b38aa4417d1658e79
Hendrik de Graaf <hendrik@dhis2.org>
2019-08-20 16:57:23 +0200

    chore(conditional): use RadioGroup in story to toggle Conditional

--- stories/Conditional.stories.js
+++ stories/Conditional.stories.js
@@ -2,22 +2,47 @@ import React from 'react'
 
 import { storiesOf } from '@storybook/react'
 
-import { File, Conditional, clear } from '../src/index.js'
+import { File, Field, RadioGroup, Conditional, clear } from '../src/index.js'
 import { createFormDecorator } from './helpers/formDecorator.js'
 
 storiesOf('Conditional fields', module)
     .addDecorator(createFormDecorator({ mutators: { clear } }))
     .add('Default', ({ formRenderProps }) => (
         <>
-            <File name="file1" label="File one" />
+            <Field
+                name="upload"
+                label="Would you like to upload a file"
+                component={RadioGroup}
+                options={[
+                    { label: 'Yes', value: true },
+                    { label: 'No', value: false },
+                ]}
+                type="select"
+                parse={value => value === 'true'}
+            />
 
             <Conditional
-                show={!!formRenderProps.values.file1}
-                fieldsToClearOnHide={['file2', 'file3']}
+                show={!!formRenderProps.values.upload}
+                fieldsToClearOnHide={['file1', 'file2']}
             >
-                <File name="file2" label="File two - cleared on hide" />
-                <File name="file3" label="File three - cleared on hide" />
-                <File name="file4" label="File four - not cleared" />
+                <Field
+                    component={File}
+                    type="file"
+                    name="file1"
+                    label="File one - cleared on hide"
+                />
+                <Field
+                    component={File}
+                    type="file"
+                    name="file2"
+                    label="File two - cleared on hide"
+                />
+                <Field
+                    component={File}
+                    type="file"
+                    name="file3"
+                    label="File three - not cleared"
+                />
                 <div>
                     <p>
                         Conditional blocks can contain random other elements too
