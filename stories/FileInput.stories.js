638efb6e6a64580fc56ae6bb0324c2e05795cfae
Hendrik de Graaf <hendrik@dhis2.org>
2019-11-19 12:18:04 +0100

    chore(file-input): cherry pick file-input work - part 2

--- stories/FileInput.stories.js
+++ stories/FileInput.stories.js
@@ -1,26 +1,53 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
 
-import { Field, FileInputAdapter, required } from '../src'
+import { Field, FileInput, required } from '../src'
 import { formDecorator } from './helpers/formDecorator'
+
+const files = [new File([], 'file1.txt'), new File([], 'file2.txt')]
 
 storiesOf('FileInput', module)
     .addDecorator(formDecorator)
     .add('Default', () => (
         <Field
-            component={FileInputAdapter}
+            component={FileInput}
             name="upload"
             label="This is a file upload"
-            multifile
-            placeholder=""
         />
     ))
     .add('Required', () => (
         <Field
-            component={FileInputAdapter}
+            component={FileInput}
             name="upload"
             label="This is a file upload"
             required
             validate={required}
+        />
+    ))
+    .add('Multifile', () => (
+        <Field
+            component={FileInput}
+            name="upload"
+            label="This is a file upload"
+            multifile
+        />
+    ))
+    .add('With values', () => (
+        <Field
+            component={FileInput}
+            name="upload"
+            label="This is a file upload"
+            required
+            multifile
+            initialValue={files}
+            validate={required}
+        />
+    ))
+    .add('Prevent placeholder', () => (
+        <Field
+            component={FileInput}
+            name="upload"
+            label="This is a file upload"
+            placeholder=""
         />
     ))
