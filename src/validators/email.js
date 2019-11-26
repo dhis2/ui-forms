61d39a797f8507272ea6a22df604ffe4d2cd587d
Jan-Gerke Salomon <jgs.salomon@gmail.com>
2019-11-26 09:40:44 +0700

    fix(email validator): remove required check

--- src/validators/email.js
+++ src/validators/email.js
@@ -3,4 +3,4 @@ import i18n from '@dhis2/d2-i18n'
 export const emailPattern = /^.+@.+\.[a-zA-Z]+$/
 export const invalidEmailMessage = i18n.t('Not a valid e-mail')
 export const email = value =>
-    emailPattern.test(value) ? undefined : invalidEmailMessage
+    value && !emailPattern.test(value) ? invalidEmailMessage : undefined
