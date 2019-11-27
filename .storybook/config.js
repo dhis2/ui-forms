631ca83a8b0ccb73aab08420a567782b4dfc5ba0
Jan-Gerke Salomon <jgs.salomon@gmail.com>
2019-11-27 17:57:34 +0700

    chore: enable testing stories

--- .storybook/config.js
+++ .storybook/config.js
@@ -5,13 +5,22 @@ import { CssResetWrapper } from './css-reset-decorator'
 
 import 'typeface-roboto'
 
+const includeTesting = 'STORYBOOK_INCLUDE_TESTING' in process.env
+
 addDecorator(jsxDecorator)
 addDecorator(withPropsTable)
 addDecorator(CssResetWrapper)
 
 function loadStories() {
-    const req = require.context('../stories', true, /\.stories\.js$/)
+    const req = require.context('../stories', true, /\.stories\.js/)
     req.keys().forEach(filename => req(filename))
 }
 
-configure(loadStories, module)
+function loadStoriesInclTesting() {
+    loadStories()
+
+    const req = require.context('../stories', true, /\.stories\.testing\.js/)
+    req.keys().forEach(filename => req(filename))
+}
+
+configure(includeTesting ? loadStoriesInclTesting : loadStories, module)
