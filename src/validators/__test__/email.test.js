363ec053a5242703f3c4902d95e6faed15bf4db1
Hendrik de Graaf <hendrik@dhis2.org>
2019-12-09 16:44:16 +0100

    refactor(email): update to case-insensitive regex and allow empty string

--- src/validators/__test__/email.test.js
+++ src/validators/__test__/email.test.js
@@ -1,11 +1,19 @@
 import { email, invalidEmailMessage } from '../email'
 
 describe('validator: email', () => {
-    it('should return undefined', () => {
+    it('should return undefined for an empty string', () => {
+        expect(email('')).toBe(undefined)
+    })
+
+    it('should return undefined for a valid email address', () => {
         expect(email('test@dhis2.org')).toBe(undefined)
     })
 
-    it('should return invalid message', () => {
+    it('should return undefined for an uppercase email address', () => {
+        expect(email('TEST@DHIS2.ORG')).toBe(undefined)
+    })
+
+    it('should return an error message for a malformed email address', () => {
         expect(email('test@dhis2.')).toBe(invalidEmailMessage)
     })
 })
--- src/validators/email.js
+++ src/validators/email.js
@@ -1,6 +1,12 @@
 import i18n from '@dhis2/d2-i18n'
 
-export const emailPattern = /^.+@.+\.[a-zA-Z]+$/
-export const invalidEmailMessage = i18n.t('Not a valid e-mail')
-export const email = value =>
-    value && !emailPattern.test(value) ? invalidEmailMessage : undefined
+const EMAIL_ADDRESS_PATTERN = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/i
+
+const invalidEmailMessage = i18n.t('Please provide a valid email address')
+
+const email = value =>
+    value && !EMAIL_ADDRESS_PATTERN.test(value)
+        ? invalidEmailMessage
+        : undefined
+
+export { email, invalidEmailMessage }
