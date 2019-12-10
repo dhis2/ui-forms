67656e909c6258f75d1ac47d5f8384a842a2937d
Hendrik de Graaf <hendrik@dhis2.org>
2019-12-10 14:29:50 +0100

    fix: amend checkbox group step description to indicate requiredness

--- cypress/integration/CheckboxGroup/common/index.js
+++ cypress/integration/CheckboxGroup/common/index.js
@@ -1,6 +1,6 @@
 import { Given } from 'cypress-cucumber-preprocessor/steps'
 
-Given('a CheckboxGroup with no selected value', () => {
+Given('a required CheckboxGroup with no selected value', () => {
     cy.visitStory('Testing:CheckboxGroup', 'Required')
     cy.verifyFormValue('choice', undefined)
 })
