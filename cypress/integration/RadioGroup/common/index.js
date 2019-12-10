e63edff7cf4322c27bb879dabc87c2c16290c469
Hendrik de Graaf <hendrik@dhis2.org>
2019-12-10 15:20:07 +0100

    chore: explain the field is required in radio-group test steps

--- cypress/integration/RadioGroup/common/index.js
+++ cypress/integration/RadioGroup/common/index.js
@@ -1,6 +1,6 @@
 import { Given } from 'cypress-cucumber-preprocessor/steps'
 
-Given('a RadioGroup with no selected value', () => {
+Given('a required RadioGroup with no selected value', () => {
     cy.visitStory('Testing:RadioGroup', 'Required')
     cy.verifyFormValue('choice', undefined)
 })
