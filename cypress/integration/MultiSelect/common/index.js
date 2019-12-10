582edde8ed2468689904a8b29b9d52f37959f355
Hendrik de Graaf <hendrik@dhis2.org>
2019-12-10 15:10:15 +0100

    chore: stop logging to the console in multi-select test

--- cypress/integration/MultiSelect/common/index.js
+++ cypress/integration/MultiSelect/common/index.js
@@ -2,6 +2,6 @@ import { Given } from 'cypress-cucumber-preprocessor/steps'
 
 Given('a required MultiSelect with no selected value', () => {
     cy.visitStory('Testing:MultiSelect', 'Required')
-    cy.getFormValue('multiSelect').then(arg => console.log('arg', arg))
+    cy.getFormValue('multiSelect')
     cy.verifyFormValue('multiSelect', undefined)
 })
