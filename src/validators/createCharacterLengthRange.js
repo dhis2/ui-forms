efe23adf7890add6980f31e0769d6618da8e20d5
Hendrik de Graaf <hendrik@dhis2.org>
2019-12-17 14:14:17 +0100

    feat(validators): introduce createMaxCharacterLength and test

--- src/validators/createCharacterLengthRange.js
+++ src/validators/createCharacterLengthRange.js
@@ -6,14 +6,16 @@ import {
     requireArgument,
 } from './helpers/index.js'
 
-const createCharacterLengthRange = (lowerBound, upperBound) => {
+const createCharacterLengthRange = (lowerBound, upperBound, customMessage) => {
     requireArgument(lowerBound, 'number')
     requireArgument(upperBound, 'number')
 
-    const errorMessage = i18n.t(
-        'Please enter between {{lowerBound}} and {{upperBound}} characters',
-        { lowerBound, upperBound }
-    )
+    const errorMessage =
+        customMessage ||
+        i18n.t(
+            'Please enter between {{lowerBound}} and {{upperBound}} characters',
+            { lowerBound, upperBound }
+        )
 
     return value =>
         isEmpty(value) ||
