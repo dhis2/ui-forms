693361fd39c61d28c48232641c01dfa59edc17a4
Hendrik de Graaf <hendrik@dhis2.org>
2019-12-17 14:26:29 +0100

    test(create-character-length-range): throws an error with invalid args

--- src/validators/__test__/createCharacterLengthRange.test.js
+++ src/validators/__test__/createCharacterLengthRange.test.js
@@ -1,9 +1,25 @@
 import { createCharacterLengthRange } from '../createCharacterLengthRange.js'
 import { testValidatorValues, allowsEmptyValues } from './helpers/index.js'
+import { requiredArgumentErrorMessage } from '../helpers/index.js'
 
 describe('validator: createCharacterLengthRange', () => {
     const betweenSixAndTenChars = createCharacterLengthRange(6, 10)
     const inValidMsg = 'Please enter between 6 and 10 characters'
+
+    it('should throw an error when lower or upper bound are not a number', () => {
+        expect(() => {
+            createCharacterLengthRange(undefined, undefined)
+        }).toThrowError(requiredArgumentErrorMessage)
+        expect(() => {
+            createCharacterLengthRange('test', 'test')
+        }).toThrowError(requiredArgumentErrorMessage)
+        expect(() => {
+            createCharacterLengthRange(1, undefined)
+        }).toThrowError(requiredArgumentErrorMessage)
+        expect(() => {
+            createCharacterLengthRange(undefined, 0)
+        }).toThrowError(requiredArgumentErrorMessage)
+    })
 
     it('should create a function', () => {
         expect(typeof betweenSixAndTenChars).toEqual('function')
