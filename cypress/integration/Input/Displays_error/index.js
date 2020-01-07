75cd2ea40943651ae7cd4960d03dce7b263ef19f
Hendrik de Graaf <HendrikThePendric@users.noreply.github.com>
2020-01-07 12:24:29 +0100

    feat: synchronous validators (#164)
    * refactor(required): use different definition of a valid value
    
    * refactor: update compose-validators test step descriptions
    
    * refactor(email): update to case-insensitive regex and allow empty string
    
    * feat(validators): introduce alphaNumeric validator and test
    
    * refactor: extract is-empty helper from required
    
    * chore(validators): fix test step descriptions for alpha-numeric
    
    * feat(validators): introduce boolean validator and tests
    
    * chore: stumped out various validators
    
    * refactor: keep all validation helpers in a single file
    
    * chore: introduce test helper to run a validator for an array of values
    
    * feat(validators): introduce alphaNumeric and test
    
    * feat(validators): introduce boolean and test
    
    * feat(validators): introduce createCharacterLengthRange and test
    
    * feat(validators): introduce createNumberRange and test
    
    * chore: fix import to restore build
    
    * refactor(validators): improve email validation logic and tests
    
    * fix(cypress): adjust error message in e2e tests so they pass again
    
    * feat(validators): introduce create-equal-to and tests
    
    * chore(validators): fix typos in the test descriptions
    
    * chore(validators): amend text in createEqualTo
    
    * feat(validators): introduce integer and tests
    
    * feat(validators): introduce number and test
    
    * feat(validators): introduce password and test
    
    * refactor(validators): extract repeated test into helper function
    
    * feat(validators): introduce createPattern and test
    
    * feat(validators): introduce internationalPhoneNumber and test
    
    * feat(validators): introduce string and test
    
    * feat(validators): introduce url and test
    
    * feat(validators): introduce username and test
    
    * refactor(validators): validate range-fn arguments and delegate to helper
    
    * feat(validators): introduce createMinNumber and test
    
    * feat(validators): introduce createMinNumber and test
    
    * feat(validators): introduce createMaxCharacterLength and test
    
    * feat(validators): introduce createMinCharacterLength and test
    
    * test(create-character-length-range): throws an error with invalid args
    
    * fix(create-equal-to): throw error for invalid argument incl test
    
    * fix(create-pattern): throw error for invalid pattern argument incl test
    
    * chore(validators): add regex attribution to url
    
    * chore(validators): removed redundant helpers
    
    * chore(validators): export validators from `./src/validators/index.js`
    
    * chore(validators): fix typo
    
    * chore(validators): fix import paths and generate translations
    
    * fix(validators): alphaNumeric should allow spaces
    
    * fix(validators): remove check on other field being empty
    
    * fix(validators): use zero as lower bound in createMaxCharacterLength
    
    * fix(validators): rename password to dhis2Password to signify intent
    
    * fix(validators): rename username to dhis2Username to signify intent
    
    * fix(validators): replace broken imports
    
    * fix(validators): only strip leading plus signs from intl phone number
    
    * refactor(validators): rename `required` to `hasValue`
    
    * fix(validators): correct broken import
    
    Co-authored-by: Jan-Gerke Salomon <Mohammer5@users.noreply.github.com>

--- cypress/integration/Input/Displays_error/index.js
+++ cypress/integration/Input/Displays_error/index.js
@@ -1,10 +1,11 @@
 import { Given, Then } from 'cypress-cucumber-preprocessor/steps'
+import { requiredMessage } from '../../../../src/validators/required.js'
 
 Given('an empty, required Input is rendered', () => {
-    cy.visitStory('Input', 'Required')
+    cy.visitStory('Testing:Input', 'Required')
     cy.verifyFormValue('agree', undefined)
 })
 
 Then('an error message is shown', () => {
-    cy.get('.error').should('contain', 'Required')
+    cy.get('.error').should('contain', requiredMessage)
 })
