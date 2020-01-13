1cd8e0bb3ea14a6c4cc3e034c824a7e36e3d1ecb
Hendrik de Graaf <hendrik@dhis2.org>
2020-01-13 17:06:16 +0100

    refactor(components): pass props explicitly in RadioGroup

--- src/components/RadioGroup.js
+++ src/components/RadioGroup.js
@@ -1,14 +1,49 @@
 import React from 'react'
+import propTypes from '@dhis2/prop-types'
 import { RadioGroupField, Radio } from '@dhis2/ui-core'
 
-import { toggleGroupOptionsProp, fieldRenderProps } from './shared/propTypes.js'
-import { normalizeProps } from './shared/helpers.js'
+import {
+    createChangeHandler,
+    createFocusHandler,
+    createBlurHandler,
+    hasError,
+    isLoading,
+    isValid,
+    getValidationText,
+} from './shared/helpers.js'
+import {
+    toggleGroupOptionsProp,
+    metaPropType,
+    inputPropType,
+} from './shared/propTypes.js'
 
-const RadioGroup = props => {
-    const { options, ...rest } = normalizeProps(props)
-
+const RadioGroup = ({
+    input,
+    meta,
+    options,
+    error,
+    showValidStatus,
+    valid,
+    validationText,
+    onBlur,
+    onFocus,
+    loading,
+    showLoadingStatus,
+    ...rest
+}) => {
     return (
-        <RadioGroupField {...rest}>
+        <RadioGroupField
+            {...rest}
+            name={input.name}
+            error={hasError(meta, error)}
+            valid={isValid(meta, valid, showValidStatus)}
+            loading={isLoading(meta, loading, showLoadingStatus)}
+            validationText={getValidationText(meta, validationText, error)}
+            onFocus={createFocusHandler(input, onFocus)}
+            onChange={createChangeHandler(input)}
+            onBlur={createBlurHandler(input, onBlur)}
+            value={input.value}
+        >
             {options.map(({ value, label }) => (
                 <Radio key={value} label={label} value={value} />
             ))}
@@ -17,9 +52,19 @@ const RadioGroup = props => {
 }
 
 RadioGroup.propTypes = {
-    ...fieldRenderProps,
-    ...RadioGroupField.propTypes,
+    input: inputPropType.isRequired,
+    meta: metaPropType.isRequired,
     options: toggleGroupOptionsProp.isRequired,
+
+    error: propTypes.bool,
+    loading: propTypes.bool,
+    showLoadingStatus: propTypes.bool,
+    showValidStatus: propTypes.bool,
+    valid: propTypes.bool,
+    validationText: propTypes.string,
+
+    onBlur: propTypes.func,
+    onFocus: propTypes.func,
 }
 
 export { RadioGroup }
